ed029847d12cae53e1e464831fc6aa379caf6fe4
Geraldi Dzakwan <geraldi.dzakwan@gmail.com>
2022-02-17 21:31:23 +0700

    Add main Katla solver (currently just the first guess) and its config

---
+++ solver/katla.py
@@ -0,0 +1,64 @@
+import json
+import random
+
+
+class Katla:
+
+    def __init__(self):
+        with open("solver/config.json", "r") as infile:
+            config = json.load(infile)
+
+        self.word_dict = []
+        with open(config["dictionary_filepath"], "r") as infile:
+            for word in infile.readlines():
+                self.word_dict.append(word.strip("\n"))
+
+        self.important_consonants = set([])
+        for char in config["important_consonants"]:
+            self.important_consonants.add(char)
+
+    @staticmethod
+    def count_vocals(word):
+        vocals = set(["a", "i", "u", "e", "o"])
+        num_vocals = 0
+
+        for char in vocals:
+            if char in word:
+                num_vocals += 1
+
+        return num_vocals
+
+    @staticmethod
+    def count_distinct_consonants(word, consonants):
+        num_distinct_consonants = 0
+
+        for char in consonants:
+            if char in word:
+                num_distinct_consonants += 1
+
+        return num_distinct_consonants
+
+    # This is to give good suggestions on word starter
+    # TO DO: Can be refined with a better logic
+    def get_starters(self):
+        starters = []
+
+        for word in self.word_dict:
+            # Exclude word that has repeatable character(s)
+            if len(set(word)) == len(word):
+                num_vocals = Katla.count_vocals(word)
+                num_distinct_consonants = Katla.count_distinct_consonants(
+                    word, self.important_consonants)
+
+                # Add as suggestion if it has more than 2 vocals and
+                # it has more than 1 important/frequent consonants
+                if num_vocals > 2 and num_distinct_consonants > 1:
+                    starters.append(word)
+
+        # Let's pick 5 at random since the total possibilites is around 70
+        return random.sample(starters, 5)
+
+
+if __name__ == '__main__':
+    katla = Katla()
+    print(katla.get_starters())
