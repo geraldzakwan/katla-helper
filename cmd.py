e11e830497a02301639c907fd7aacfc60fe2a274
Geraldi Dzakwan <geraldi.dzakwan@gmail.com>
2022-02-19 17:29:46 +0700

    Modify copywritings

--- cmd.py
+++ cmd.py
@@ -10,12 +10,15 @@ def main():
     katla = Katla()
     states = []
 
-    for i in range(0, 6):
+    turn = 0
+    finish = False
+
+    while turn < 6 and not finish:
         print_dashes(50)
-        print("Langkah {} dari 6".format(i + 1))
+        print("Langkah {} dari 6".format(turn + 1))
         print_dashes(50)
 
-        if i == 0:
+        if turn == 0:
             print("Ini beberapa kata bagus sebagai pemulai:")
             print(katla.get_starters())
             print_dashes(50)
@@ -24,7 +27,7 @@ def main():
 
         while not is_guess_valid:
             guess = Katla.clean(
-                input("Ketik kata yang kamu masukkan ke Katla: "))
+                input("Ketik kata yang ingin kamu masukkan ke Katla: "))
 
             if len(guess) == 5:
                 if katla.is_kbbi_word(guess):
@@ -37,29 +40,47 @@ def main():
                 print("Kata harus punya lima huruf, harap ulangi ya!")
                 print_dashes(50)
 
-        print(
-            "Sekarang, coba beritahu petunjuk warna yang kamu dapatkan dari Katla"
-        )
-        print(
-            "Sebagai contoh, jika warnanya [hijau, abu-abu, kuning, abu-abu, hijau], harap masukkan: hakah"
-        )
+        if turn == 0:
+            print(
+                "Sekarang, coba beritahu petunjuk warna yang kamu dapatkan dari Katla"
+            )
+            print(
+                "Sebagai contoh, ketik \"hakah\" jika warnanya [hijau, abu-abu, kuning, abu-abu, hijau]"
+            )
+            print(
+                "Jika kata kamu sudah benar, masukkan \"hhhhh\" atau tekan Ctrl + C untuk keluar dari program"
+            )
+            print_dashes(75)
 
-        verdict = input("Petunjuk Katla untuk tebakan '{}': ".format(guess))
+        verdict = input("Petunjuk Katla untuk tebakan \"{}\": ".format(guess))
         print_dashes(50)
 
         if verdict == "hhhhh":
             print("Brilian!")
             print_dashes(50)
 
-            break
+            finish = True
         else:
-            states.append({
-                "word": guess,
-                "verdict": verdict
-            })
+            states.append({"word": guess, "verdict": verdict})
+            guesses = katla.get_guesses(states)
 
-            print("Berikut beberapa rekomendasi untuk tebakan selanjutnya:")
-            print(katla.get_guesses(states))
+            if len(guesses) > 0:
+                if len(guesses) == 1:
+                    print("Jawabannya pasti \"{}\"!".format(guesses[0]))
+                else:
+                    print(
+                        "Berikut beberapa rekomendasi untuk tebakan selanjutnya:"
+                    )
+                    print(guesses)
+            else:
+                print(
+                    "Mohon maaf kami sekarang tidak menemukan kata apapun yang cocok dari kamus kami :)"
+                )
+                print_dashes(75)
+
+                finish = True
+
+        turn = turn + 1
 
 
 if __name__ == '__main__':
